f41d7157806632104163e0917c4cbfe47aabdd74
Dmitriy <dmitriy.shchebetovskyi@gmail.com>
2021-01-18 23:54:33 +0200

    Added ReviewController.

---
+++ DoctorAppointmentScheduling.WebApi/Controllers/ReviewController.cs
@@ -0,0 +1,48 @@
+﻿using DoctorAppointmentScheduling.Domain.Interfaces;
+using DoctorAppointmentScheduling.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorAppointmentScheduling.WebAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewRepository _reviewRepository;
+
+        public ReviewController(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+
+        // POST api/<ReviewController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Review review)
+        {
+            var newReviewID = _reviewRepository.Add(review);
+            var newReview = _reviewRepository.GetReviews(newReviewID);
+
+            return Created($"api/<ReviewController>/{newReviewID}", newReview);
+        }
+
+        [HttpGet("doctor/{id}")]
+        public IActionResult GetDoctorReviews(int id)
+        {
+            return Ok(_reviewRepository.GetReviews(id));
+        }
+
+        [HttpPut()]
+        public IActionResult Update([FromBody] Review review)
+        {
+            _reviewRepository.Update(review);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _reviewRepository.Delete(id);
+            return Ok();
+        }
+    }
+}
