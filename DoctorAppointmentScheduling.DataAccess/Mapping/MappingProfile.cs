bb380518986dc3f7b416f9fa6c7556a23d794911
Dmitriy <dmitry.shebetovsky@gmail.com>
2021-04-09 00:28:42 +0300

    Fixed mappings.

---
+++ DoctorAppointmentScheduling.DataAccess/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DoctorAppointmentScheduling.DataAccess.Dtos;
+using DoctorAppointmentScheduling.Domain.Entities;
+
+namespace DoctorAppointmentScheduling.WebAPi.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AppointmentDto, Appointment>().ReverseMap();
+            CreateMap<DoctorDto, Doctor>().ReverseMap();
+            CreateMap<PatientDto, Patient>().ReverseMap();
+            CreateMap<ReviewDto, Review>().ReverseMap();
+        }
+    }
+}
---
+++ DoctorAppointmentScheduling.WebApi/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DoctorAppointmentScheduling.Domain.Entities;
+using DoctorAppointmentScheduling.WebAPi.ViewModels;
+
+namespace DoctorAppointmentScheduling.WebAPi.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AppointmentViewModel, Appointment>().ReverseMap();
+            CreateMap<DoctorViewModel, Doctor>().ReverseMap();
+            CreateMap<PatientViewModel, Patient>().ReverseMap();
+            CreateMap<ReviewViewModel, Review>().ReverseMap();
+        }
+    }
+}
